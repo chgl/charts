e3fd50fe716dfb1ce48ed7c7c2ef8688d9ee8a9f
chgl <chgl@users.noreply.github.com>
2024-10-13 14:32:35 +0200

    fix args and shfmt

--- .github/renovate-bump-version.sh
+++ .github/renovate-bump-version.sh
@@ -2,20 +2,20 @@
 
 depName="${1}"
 if [ -z "${depName}" ]; then
-    echo "Missing argument 'depName'" >&2
-    exit 1
+  echo "Missing argument 'depName'" >&2
+  exit 1
 fi
 
 newVersion="${2}"
 if [ -z "${newVersion}" ]; then
-    echo "Missing argument 'newVersion'" >&2
-    exit 1
+  echo "Missing argument 'newVersion'" >&2
+  exit 1
 fi
 
 git config --global --add safe.directory /root/workspace
 
 CHANGELOG_ENTRY=$(
-    cat <<EOF
+  cat <<EOF
 # When using the list of objects option the valid supported kinds are
 # added, changed, deprecated, removed, fixed and security.
 - kind: changed
@@ -28,18 +28,18 @@ export CHANGELOG_ENTRY
 CHANGED_CHARTS=$(ct list-changed --config=.github/ct/ct.yaml)
 
 for CHART in $CHANGED_CHARTS; do
-    echo "$CHART: bumping Chart.yaml version"
-    # Bump the chart version by one patch version
-    # via <https://github.com/argoproj/argo-helm/blob/main/scripts/renovate-bump-version.sh>
-    version=$(grep '^version:' "${CHART}/Chart.yaml" | awk '{print $2}')
-    echo "$CHART: current: ${version}"
-    major=$(echo "${version}" | cut -d. -f1)
-    minor=$(echo "${version}" | cut -d. -f2)
-    patch=$(echo "${version}" | cut -d. -f3)
-    patch=$((patch + 1))
-    echo "$CHART: next: ${version}"
-    sed -i "s/^version:.*/version: ${major}.${minor}.${patch}/g" "${CHART}/Chart.yaml"
+  echo "$CHART: bumping Chart.yaml version"
+  # Bump the chart version by one patch version
+  # via <https://github.com/argoproj/argo-helm/blob/main/scripts/renovate-bump-version.sh>
+  version=$(grep '^version:' "${CHART}/Chart.yaml" | awk '{print $2}')
+  echo "$CHART: current: ${version}"
+  major=$(echo "${version}" | cut -d. -f1)
+  minor=$(echo "${version}" | cut -d. -f2)
+  patch=$(echo "${version}" | cut -d. -f3)
+  patch=$((patch + 1))
+  echo "$CHART: next: ${version}"
+  sed -i "s/^version:.*/version: ${major}.${minor}.${patch}/g" "${CHART}/Chart.yaml"
 
-    echo "$CHART: updating Chart.yaml with auto-generated changelog annotation: Updated ${depName} to ${newVersion}"
-    yq -i '.annotations."artifacthub.io/changes" = strenv(CHANGELOG_ENTRY)' "$CHART/Chart.yaml"
+  echo "$CHART: updating Chart.yaml with auto-generated changelog annotation: Updated ${depName} to ${newVersion}"
+  yq -i '.annotations."artifacthub.io/changes" = strenv(CHANGELOG_ENTRY)' "$CHART/Chart.yaml"
 done
--- .github/renovate-post-upgrade.sh
+++ .github/renovate-post-upgrade.sh
@@ -4,16 +4,16 @@
 
 depName="${1}"
 if [ -z "${depName}" ]; then
-    echo "Missing argument 'depName'" >&2
-    exit 1
+  echo "Missing argument 'depName'" >&2
+  exit 1
 fi
 
 newVersion="${2}"
 if [ -z "${newVersion}" ]; then
-    echo "Missing argument 'newVersion'" >&2
-    exit 1
+  echo "Missing argument 'newVersion'" >&2
+  exit 1
 fi
 
 echo "Changed dep name is: $depName to $newVersion"
 
-docker run --rm -v "${PWD}:/root/workspace" ghcr.io/chgl/kube-powertools:v2.3.25@sha256:99b5cc7a49cd443fb953ca4ab52dc45245a5c43ad03e3503be06d0d0f512b67d /root/workspace/.github/renovate-bump-version.sh
+docker run --rm -v "${PWD}:/root/workspace" ghcr.io/chgl/kube-powertools:v2.3.25@sha256:99b5cc7a49cd443fb953ca4ab52dc45245a5c43ad03e3503be06d0d0f512b67d /root/workspace/.github/renovate-bump-version.sh ${depName} ${newVersion}
